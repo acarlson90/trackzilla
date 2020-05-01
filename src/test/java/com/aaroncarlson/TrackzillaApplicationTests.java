d59b0184e96e1f948181df4afb50b3ffe4513435
Aaron Carlson <acarlson90@gmail.com>
2020-04-30 21:42:15 -0700

    Initial Commit

---
+++ src/test/java/com/aaroncarlson/TrackzillaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaroncarlson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackzillaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
