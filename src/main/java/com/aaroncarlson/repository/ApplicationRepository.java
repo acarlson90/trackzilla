8bdd89b123ea6fd969ac141767f4db414e76b631
Aaron Carlson <acarlson90@gmail.com>
2020-05-03 21:55:41 -0700

    Application, Release and Tracking Entity and Repositories

---
+++ src/main/java/com/aaroncarlson/repository/ApplicationRepository.java
@@ -0,0 +1,9 @@
+package com.aaroncarlson.repository;
+
+import com.aaroncarlson.model.Application;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ApplicationRepository extends JpaRepository<Application, Long> {
+}
---
+++ src/main/java/com/aaroncarlson/repository/ReleaseRepository.java
@@ -0,0 +1,9 @@
+package com.aaroncarlson.repository;
+
+import com.aaroncarlson.model.Release;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReleaseRepository extends JpaRepository<Release, Long> {
+}
